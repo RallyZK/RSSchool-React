22008273ccb679129395f25d06b1adfbf7fc2bb8
Rallyzk <railyabalakaeva@gmail.com>
2023-03-20 13:34:43 +0300

    fix: fix searchbar

--- components/src/components/SearchBar/SearchBar.tsx
+++ components/src/components/SearchBar/SearchBar.tsx
@@ -1,38 +1,57 @@
 import './SearchBar.css';
 import React, { Component } from 'react';
 
-class SearchBar extends Component {
-  state = {
-    searchPhrase: '',
-  };
+type SearchBarState = {
+  searchPhrase: string;
+};
+
+type SearchBarProps = {
+  smth?: string;
+};
+
+class SearchBar extends Component<SearchBarProps, SearchBarState> {
+  constructor(props: SearchBarProps) {
+    super(props);
+    this.state = {
+      searchPhrase: '',
+    };
+  }
+
   myRef = React.createRef<HTMLInputElement>();
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const input = event.target;
-    const value = input.value;
-    this.setState({ searchPhrase: value });
+  saveToLS = () => {
+    localStorage.setItem('searchPhraseToLS', this.state.searchPhrase);
   };
 
-  handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const searchPhraseToLS = this.state.searchPhrase;
-    localStorage.setItem('searchPhraseToLS', searchPhraseToLS);
-  };
+  componentWilldMount() {
+    this.saveToLS();
+  }
 
   componentDidMount() {
     const searchPhraseFromLS = localStorage.getItem('searchPhraseToLS');
     this.setState({ searchPhrase: searchPhraseFromLS || '' });
   }
 
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchPhrase: event.target.value });
+    this.saveToLS();
+  };
+
+  handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    this.saveToLS();
+  };
+
   render() {
     return (
       <form className='search-bar' onSubmit={(event) => this.handleFormSubmit(event)}>
         <input
           ref={this.myRef}
+          type='text'
+          placeholder='Enter something'
           className='search-input'
           value={this.state.searchPhrase}
-          onChange={this.handleChange}
-          placeholder='Enter something'
+          onChange={(event) => this.handleChange(event)}
         ></input>
         <button className='search-button' type='submit'></button>
       </form>
