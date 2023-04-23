2918d5f8520dd2b8828c90d09df16769a451a36b
Railya Balakaeva <railyabalakaeva@gmail.com>
2023-04-23 19:00:51 +0300

    refactor

--- myReactApp/server/index.js
+++ myReactApp/server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import App from '../App';
+import App from '../src/App';
 import fs from 'fs';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
